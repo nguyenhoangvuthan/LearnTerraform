e2702827fe2b8636003434e84b90ce9f986f1fb5
Nguyen Hoang Vu Than <nguyenhoangvuthan@gmail.com>
2024-02-28 11:48:04 +0700

    Second Commit for Exercise 6

--- terraform-scripts/exercise6/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terra-state-dove-nhvthan"
-    key = "terraform/backend"
-    region = "us-east-1"
-  }
-}
--- terraform-scripts/exercise6/vpc.tf
+++ terraform-scripts/exercise6/vpc.tf
@@ -68,7 +68,7 @@ resource "aws_subnet" "dove-priv-3" {
   }
 }
 
-resource "aws_route_table" "dove-IGW" {
+resource "aws_internet_gateway" "dove-IGW" {
   vpc_id = aws_vpc.dove.id
   tags = {
     Name = "dove-IGW"
@@ -76,12 +76,27 @@ resource "aws_route_table" "dove-IGW" {
 }
 
 resource "aws_route_table" "dove-pub-RT" {
-  vpc_id = aws_instance.dove.id
-  route = {
-    cird_block = "0.0.0.0/0"
+  vpc_id = aws_vpc.dove.id
+  route {
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.dove-IGW.id
   }
   tags = {
     Name = "dove-pub-RT"
   }
+}
+
+resource "aws_route_table_association" "dove-pub-1-a" {
+  subnet_id = aws_subnet.dove-pub-1.id
+  route_table_id = aws_route_table.dove-pub-RT.id
+}
+
+resource "aws_route_table_association" "dove-pub-2-a" {
+  subnet_id = aws_subnet.dove-pub-2.id
+  route_table_id = aws_route_table.dove-pub-RT.id
+}
+
+resource "aws_route_table_association" "dove-pub-3-a" {
+  subnet_id = aws_subnet.dove-pub-3.id
+  route_table_id = aws_route_table.dove-pub-RT.id
 }
